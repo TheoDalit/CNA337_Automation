7ae03b4f3dea1f521a8f8d47c554cd09413095b1
TheoDalit <theodalit@gmail.com>
2020-12-10 21:23:44 -0800

    Automate update & upgrade

--- Server.py
+++ Server.py
@@ -1,4 +1,5 @@
 import os
+import paramiko
 
 class Server:
     """ Server class for representing and manipulating servers. """
@@ -11,3 +12,16 @@ class Server:
         # TODO - Use os module to ping the server
         response = os.system("ping -n 4 " + self.server_ip) #https://stackoverflow.com/questions/2953462/pinging-servers-in-python
         return self.server_ip
+
+ssh = paramiko.SSHClient() # https://mainlydata.kubadev.com/python/using-paramiko-to-control-an-ec2-instance/
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect('3.139.89.21', username='ec2-user', password='', key_filename=r'C:\Users\hombo\.ssh\id_rsa') # https://stackoverflow.com/questions/37400974/unicode-error-unicodeescape-codec-cant-decode-bytes-in-position-2-3-trunca
+stdin, stdout, stderr = ssh.exec_command('sudo yum update; sudo yum upgrade')
+stdin.flush()
+
+data = stdout.read().splitlines()
+for line in data:
+    print(line)
+
+ssh.close()
+
--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 from Server import Server
 def print_program_info():
     # TODO - Change your name
-    print("Server Automator v0.1 by Theo")
+    print("Server Automator v0.2 by Theo")
 
 # This is the entry point to our program
 if __name__ == '__main__':
@@ -13,4 +13,5 @@ if __name__ == '__main__':
     # TODO - Create a Server object
     ec2 = Server('3.139.89.21')
     # TODO - Call Ping method and print the results
-    print(ec2.ping())
+    # print(ec2.ping())
+
